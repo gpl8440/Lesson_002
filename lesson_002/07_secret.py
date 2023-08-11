d983e351fac6079b1bfc220340d809ea51a66b92
gpl8440 <89629238+gpl8440@users.noreply.github.com>
2023-08-11 20:47:44 +0400

    Secret solved

--- lesson_002/07_secret.py
+++ lesson_002/07_secret.py
@@ -21,4 +21,10 @@ secret_message = [
 # Требуется задать конкретные индексы, например secret_message[3][12:23:4]
 # Если нужны вычисления и разные пробы - делайте это в консоли пайтона, тут нужен только результат
 
-# TODO вывести расшифрованное сообщение
+ 
+mes1 = secret_message[0][3]
+mes2 = secret_message[1][9:13]
+mes3 = secret_message[2][5:15:2]
+mes4 = secret_message[3][12:6:-1]
+mes5 = secret_message[4][20:15:-1]
+print(mes1, mes2, mes3, mes4, mes5)
