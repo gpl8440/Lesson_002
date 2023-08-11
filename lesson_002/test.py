ad077640e365a48a8afc42a57e5ac32ed7a3c2c0
gpl8440 <89629238+gpl8440@users.noreply.github.com>
2023-08-11 17:37:33 +0400

    Examle was changed

--- lesson_002/test.py
+++ lesson_002/test.py
@@ -1,45 +1,33 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Есть словарь координат городов
+# Создайте списки:
 
-sites = {
-    'Moscow': (550, 370),
-    'London': (510, 510),
-    'Paris': (480, 480),
-}
+# моя семья (минимум 3 элемента, есть еще дедушки и бабушки, если что)
+my_family = ['мама', 'папа',  'жена', 'дочь']
 
-# Составим словарь словарей расстояний между ними
-# расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+# список списков приблизителного роста членов вашей семьи
+my_family_height = [
+    # ['имя', рост],
+    ['Люда',160],
+    ['Игорь',170],
+    ['Марина',176],
+    ['Анна',90]
+]
 
-distances = {'Moscow': {'London': round(((sites['Moscow'][0] - sites['London'][0]) ** 2
-                                         + (sites['Moscow'][1] - sites['London'][1]) ** 2) ** 0.5
-                                        , 2),
-                        'Paris': round(((sites['Moscow'][0] - sites['Paris'][0]) ** 2
-                                        + (sites['Moscow'][1] - sites['Paris'][1]) ** 2) ** 0.5
-                                       , 2),
-                        },
-             'London': {'Moscow': round(((sites['London'][0] - sites['Moscow'][0]) ** 2
-                                         + (sites['London'][1] - sites['Moscow'][1]) ** 2) ** 0.5
-                                        , 2),
+# Выведите на консоль рост отца в формате
+#   Рост отца - ХХ см
 
-                        'Paris': round(((sites['London'][0] - sites['Paris'][0]) ** 2
-                                        + (sites['London'][1] - sites['Paris'][1]) ** 2) ** 0.5
-                                       , 2),
-                        },
+print('Рост: '+my_family[1]+' -', my_family_height[1][1],'см.')
 
-             'Paris': {'Moscow': round(((sites['Paris'][0] - sites['Moscow'][0]) ** 2
-                                        + (sites['Paris'][1] - sites['Moscow'][1]) ** 2) ** 0.5
-                                       , 2),
+# Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
+#   Общий рост моей семьи - ХХ см
 
-                       'London': round(((sites['Paris'][0] - sites['London'][0]) ** 2
-                                        + (sites['Paris'][1] - sites['London'][1]) ** 2) ** 0.5
-                                       , 2),
-                       }
-             }
+sum_height = 0
+sum_height += my_family_height[0][1]
+sum_height += my_family_height[1][1]
+sum_height += my_family_height[2][1]
+sum_height += my_family_height[3][1]
 
-print(distances)
-
-# Зачет!
+print('Общий рост моей семьи -', sum_height, 'см.')
